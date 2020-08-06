b60981c7e3f01ceef12b8115d99d03e9eabc5bf7
muta93srb <muta93srb@hotmail.com>
2020-08-07 01:49:34 +0200

    Test commit for auto deploy

--- project.clj
+++ project.clj
@@ -1,28 +1,22 @@
-(defproject pedestal-api "0.3.5-SNAPSHOT"
+(defproject pedestal-api "0.3.5"
   :description "A batteries-included API for Pedestal using Swagger"
   :url "https://github.com/oliyh/pedestal-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[frankiesardo/route-swagger "0.1.4"]]
-  :repl-options {:init-ns user}
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.8.0"]
-                                       [io.pedestal/pedestal.service "0.5.3"]]}
-             :dev {:source-paths ["dev" "example"]
-                   :resources ["dev-resources"]
-                   :dependencies [[potemkin "0.4.3"]
-                                  [org.clojure/tools.namespace "0.2.10"]
-                                  [io.pedestal/pedestal.jetty "0.5.3"]
-                                  [clj-http "3.4.1"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [frankiesardo/route-swagger "0.1.4"]
+                 [io.pedestal/pedestal.service "0.5.8"]
+                 [io.pedestal/pedestal.jetty "0.5.8"]
 
-                                  [ch.qos.logback/logback-classic "1.1.8"
-                                   :exclusions [org.slf4j/slf4j-api]]
-                                  [org.slf4j/jcl-over-slf4j "1.7.22"]
-                                  [org.slf4j/jul-to-slf4j "1.7.22"]
-                                  [org.slf4j/log4j-over-slf4j "1.7.22"]]}}
+                 [potemkin "0.4.5"]
+                 [org.clojure/tools.namespace "1.0.0"]
+                 [clj-http "3.10.1"]
 
-  ;; for heroku deployment of example
-  :min-lein-version "2.0.0"
-  :plugins [[lein-sub "0.3.0"]]
-  :sub ["example"]
-  :aliases {"uberjar" ["sub" "uberjar"]}
-  :uberjar-name "")
+                 [ch.qos.logback/logback-classic "1.2.3"
+                  :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
+                 [org.slf4j/log4j-over-slf4j "1.7.30"]]
+  :plugins [[s3-wagon-private "1.3.4"]]
+  :repositories [["muta93srb-maven" {:url "s3p://muta93srb-maven/releases/"
+                                     :creds :gpg}]])
